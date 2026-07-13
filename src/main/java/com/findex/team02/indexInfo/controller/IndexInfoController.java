1e23a8896c40e57d9b235c6d1561ef5b6d3297b5
줴령 <gpfud09@gmail.com>
2026-07-13 20:40:23 +0900

    feat: 지수 정보 목록 조회 Controller 구현

--- src/main/java/com/findex/team02/indexInfo/controller/IndexInfoController.java
+++ src/main/java/com/findex/team02/indexInfo/controller/IndexInfoController.java
@@ -1,12 +1,11 @@
 package com.findex.team02.indexInfo.controller;
 
+import com.findex.team02.indexInfo.dto.request.IndexInfoSearchRequest;
+import com.findex.team02.indexInfo.dto.response.CursorPageResponseIndexInfoDto;
 import com.findex.team02.indexInfo.service.IndexInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +15,11 @@ public class IndexInfoController {
     private final IndexInfoService indexInfoService;
 
     // 지수 정보 목록 조회
-//    @GetMapping("/")
-//    public ResponseEntity<> getIndexInfos(@RequestParam(required = false) String indexClassification,
-//                                          @RequestParam(required = false) String indexName,
-//                                          @RequestParam(required = false) Boolean favorite,
-//                                          @RequestParam(required = false) Long idAfter,
-//                                          @RequestParam(required = false) String cursor,
-//                                          @RequestParam(defaultValue = "indexClassification") String sortField,
-//                                          @RequestParam(defaultValue = "asc") String sortDirection,
-//                                          @RequestParam(defaultValue = "10") Integer size) {
-//
-//
-//    }
+    @GetMapping
+    public ResponseEntity<CursorPageResponseIndexInfoDto> getIndexInfos(@ModelAttribute IndexInfoSearchRequest request) {
+        CursorPageResponseIndexInfoDto response = indexInfoService.getIndexInfos(request);
+
+        return ResponseEntity.ok(response);
+    }
 
 }
