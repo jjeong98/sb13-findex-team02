e0ed48636f121617507f70357f7c6e75768d4ea8
줴령 <gpfud09@gmail.com>
2026-07-13 20:39:30 +0900

    feat: 지수 정보 목록 조회 Service 구현

--- src/main/java/com/findex/team02/indexInfo/service/BasicIndexInfoService.java
+++ src/main/java/com/findex/team02/indexInfo/service/BasicIndexInfoService.java
@@ -1,13 +1,48 @@
 package com.findex.team02.indexInfo.service;
 
+import com.findex.team02.indexInfo.dto.request.IndexInfoSearchRequest;
+import com.findex.team02.indexInfo.dto.response.CursorPageResponseIndexInfoDto;
+import com.findex.team02.indexInfo.dto.response.IndexInfoDto;
+import com.findex.team02.indexInfo.entity.IndexInfo;
+import com.findex.team02.indexInfo.mapper.IndexInfoMapper;
 import com.findex.team02.indexInfo.repository.IndexInfoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BasicIndexInfoService implements IndexInfoService {
 
     private final IndexInfoRepository indexInfoRepository;
+    private final IndexInfoMapper indexInfoMapper;
+
+    @Override
+    public CursorPageResponseIndexInfoDto getIndexInfos(IndexInfoSearchRequest request) {
+        int size = request.size();
+
+        List<IndexInfo> indexInfos = indexInfoRepository.findAllByCondition(request);
+
+        boolean hasNext = indexInfos.size() > size;
+
+        if (hasNext) {
+            indexInfos.remove(size);
+        }
+
+        List<IndexInfoDto> content = indexInfoMapper.toDto(indexInfos);
+
+        IndexInfo last = indexInfos.get(indexInfos.size() - 1);
+
+        return new CursorPageResponseIndexInfoDto(
+                content,
+                last.getId().toString(),
+                last.getId(),
+                size,
+                indexInfos.size(),
+                hasNext
+        );
+    }
 
 }
--- src/main/java/com/findex/team02/indexInfo/service/IndexInfoService.java
+++ src/main/java/com/findex/team02/indexInfo/service/IndexInfoService.java
@@ -1,4 +1,13 @@
 package com.findex.team02.indexInfo.service;
 
+import com.findex.team02.indexInfo.dto.request.IndexInfoSearchRequest;
+import com.findex.team02.indexInfo.dto.response.CursorPageResponseIndexInfoDto;
+import com.findex.team02.indexInfo.dto.response.IndexInfoDto;
+
+import java.util.List;
+
 public interface IndexInfoService {
+
+    CursorPageResponseIndexInfoDto getIndexInfos(IndexInfoSearchRequest request);
+
 }
